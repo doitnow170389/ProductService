9b2f7c2439520574fe27f166044287b2b439f520
Shashank Sharma <shashank.doitnow@gmail.com>
2023-12-07 14:49:54 +0530

    Changes

--- ProductInfromationService/src/main/java/com/productInformation/controller/ProductController.java
+++ ProductInfromationService/src/main/java/com/productInformation/controller/ProductController.java
@@ -1,14 +1,44 @@
 package com.productInformation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.productInformation.dto.CategoryRequest;
+import com.productInformation.model.Category;
+import com.productInformation.model.Product;
+import com.productInformation.service.CategoryService;
+import com.productInformation.service.ProductService;
 
 @RestController
 public class ProductController {
 
-	@GetMapping("/hello")
-	String sayhello()
+	@Autowired
+	CategoryService cService;
+	@Autowired
+	ProductService pService;
+	
+	@PostMapping("/SKUCategory")
+	public ResponseEntity<String> saveCategory(@RequestBody CategoryRequest cRequest)
 	{
-		return "Hello Dear";
+//		ProductCategory pCategory=new ProductCategory(cRequest);
+//		pCategory=cService.saveSKUCategory(pCategory);
+//		for(String s : cRequest.getProductinformation())
+//		{
+//			ProductInformation pInfo=new ProductInformation();
+//			pInfo.setSKU_Name(s);
+////			pInfo.setSKU_URL(s);
+//			pInfo.setProductCategory(pCategory);
+//			pService.saveSKUName(pInfo);
+//		}
+		System.out.println("Value-   "+cRequest.getPCategory().getSKUCategory_Name());
+		cService.saveSKUCategory(cRequest.getPCategory());
+		return new ResponseEntity<String>("Record Saved Successfully", HttpStatus.CREATED);
+		
 	}
 }
