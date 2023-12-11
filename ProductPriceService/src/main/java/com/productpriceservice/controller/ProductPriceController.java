8a6ffb5ee65cddb055d59af9247dc84d076a227d
Shashank Sharma <shashank.doitnow@gmail.com>
2023-12-11 14:37:30 +0530

    None

--- ProductPriceService/src/main/java/com/productpriceservice/controller/ProductPriceController.java
+++ ProductPriceService/src/main/java/com/productpriceservice/controller/ProductPriceController.java
@@ -1,14 +1,9 @@
 package com.productpriceservice.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.productpriceservice.exception.ProductPriceNotFoundException;
